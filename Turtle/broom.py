47240e62b8401422fe853437d4746ab559d37030
Zeki Bene <zackbene9@gmail.com>
2023-04-16 00:02:26 +0200

    work on it tmrw

--- Turtle/broom.py
+++ Turtle/broom.py
@@ -1,18 +1,33 @@
 import turtle
+import random
 
 def drawBroom(broomLength: int, bristlesNum: int, bristleLength: int) -> None:
+    turtle.penup()
+    randX = random.randint(-50,200)
+    randY = random.randint(-200, 200)
+    turtle.goto(randX, randY)
+
+    turtle.pendown()
     turtle.fd(broomLength)
 
-    for _ in range(bristlesNum):
+    turnDegree = 45
+
+    for i in range(bristlesNum):
         turtle.pendown()
-        turtle.lt(25)
-        turtle.fd(bristleLength)
-        turtle.penup()
-        turtle.lt(180)
-        turtle.fd(bristleLength)
-        turtle.lt(180)
+        if i == 0:
+            turtle.fd(bristleLength)
+            turtle.penup()
+            turtle.lt(180)
+            turtle.fd(bristleLength)
+            turtle.lt(180)
+        else:
+            turtle.lt(turnDegree)
+            turtle.fd(bristleLength)
+
 
 if __name__ == "__main__":
+    sc = turtle.Screen()
+    sc.setup(640, 480)
     turtle = turtle.Turtle()
     turtle.speed(1)
     drawBroom(100, 5, 50)
