484c7e5437c9262ac034b14be04fb44bdaeb3262
Zeki Bene <zackbene9@gmail.com>
2023-04-16 15:15:40 +0200

    Click and check it functions complete.

--- Tkinter/aimLabs.py
+++ Tkinter/aimLabs.py
@@ -8,18 +8,40 @@ class TargetCanvas(Canvas):
         self.pack()
         
         #Variables
+        self.parent = parent
+        self.targetX1 = 250
+        self.targetY1 = 250
 
         #Get mouse position
         self.bind_all("<Motion>", self.getMousePos)
         self.bind_all("<Button-1>", self.click)
+        self.parent.after(800, self.genTarget)
 
-    """ Get mouse x and y """
+    """ Gen target """
+    def genTarget(self) -> None:
+        self.target = self.create_rectangle(self.targetX1, self.targetY1, self.targetX1 + 50, self.targetY1 + 50, fill="red")
+
+    """ Mouse functions """
+    #Get mouse x and y
     def getMousePos(self, event) -> None:
         self.mouseX = event.x
         self.mouseY = event.y
 
+    #Left click function
     def click(self, event) -> None:
         print(self.mouseX, self.mouseY)
+
+        if self.checkHit() == True:
+            print("Hit")
+        else:
+            print("Miss")
+
+    """ Check for hit """
+    def checkHit(self) -> bool:
+        if self.mouseX > self.targetX1 and self.mouseX < self.targetX1+50 and self.mouseY > self.targetY1 and self.mouseY < self.targetY1+50:
+            return True
+        else:
+            return False
 
 """ Main driver """
 if __name__ == "__main__":
